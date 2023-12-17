7703a90c241e054f5e335dd4e4ccde59e9b582e8
Avijayasundar <115879213+Avijayasundar@users.noreply.github.com>
2023-12-17 12:26:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo(){
+  var res1 = document.getElementById("Firstname").value;
+  console.log(res1);
+ 
+    var res2 = document.getElementById("Middlename").value;
+    console.log(res2);
+    
+      var res3 = document.getElementById("Lastname").value;
+      console.log(res3);
+     
+        var res = document.getElementById("email").value;
+        console.log(res);
+        }
